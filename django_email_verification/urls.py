135104bba986a1602e62529bb0112a565c654b99
oio0-0 <lolkin2268@gmail.com>
2026-04-27 19:48:06 +0300

    Update urls.py

--- django_email_verification/urls.py
+++ django_email_verification/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import verify_email_page, verify_password_page
+from .views import verify_email_page, verify_password_page, verify_email_update_page
 
 urlpatterns = [
+    path('email-update/<str:token>', csrf_exempt(verify_email_update_page)),
     path('email/<str:token>', csrf_exempt(verify_email_page)),
     path('password/<str:token>', csrf_exempt(verify_password_page)),
 ]
