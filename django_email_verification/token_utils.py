b5745071d92e95299144c03dfbc9efd83da72ce1
oio0-0 <lolkin2268@gmail.com>
2026-04-27 19:56:09 +0300

    Update token_utils.py

--- django_email_verification/token_utils.py
+++ django_email_verification/token_utils.py
@@ -4,7 +4,6 @@ import jwt
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-
 class EmailVerificationTokenGenerator:
     """
     Strategy object used to generate and check tokens for the password
@@ -51,7 +50,7 @@ class EmailVerificationTokenGenerator:
         """
 
         try:
-            payload = jwt.decode(token, self.secret, algorithms=['HS256'])
+            payload = jwt.decode(token, self.secret, algorithms=['HS256']) #автоматическая проверка на то, что токен не истёк
             email, exp = payload['email'], payload['exp']
 
             for k, v in kwargs.items():
@@ -59,7 +58,7 @@ class EmailVerificationTokenGenerator:
                     return False, None
 
             if hasattr(settings, 'EMAIL_MULTI_USER') and settings.EMAIL_MULTI_USER:
-                users = get_user_model().objects.filter(email=email)
+                users = get_user_model().objects.filter(email=email) 
             else:
                 users = [get_user_model().objects.get(email=email)]
         except (ValueError, get_user_model().DoesNotExist, jwt.DecodeError, jwt.ExpiredSignatureError):
@@ -69,7 +68,41 @@ class EmailVerificationTokenGenerator:
             return False, None
 
         return True, users[0]
+    
+    def check_token_update_email(self, token, **kwargs):
+        """      
+        Check that a password reset token is correct.
+        Args:
+            token (str): the token from the url
+            kwargs: the extra required payload
 
+        Returns:
+            (tuple): tuple containing:
+                valid (bool): True if the token is valid
+                user (Model): the user model if the token is valid
+                new_email (str): the new user email
+        """
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=['HS256'])
+            old_email, new_email, exp = payload['email'], payload['new_email'], payload['exp']
+            
+            if not new_email:
+                return False, None, None
+
+            for k, v in kwargs.items():
+                if payload[k] != v:
+                    return False, None, None
+
+            try:
+                user = get_user_model().objects.get(email=old_email)
+            except get_user_model().DoesNotExist:
+                return False, None, None
+
+        except (ValueError, jwt.DecodeError, jwt.ExpiredSignatureError):
+            return False, None, None
+
+        return True, user, new_email
+    
     @staticmethod
     def now():
         return datetime.now().timestamp()
