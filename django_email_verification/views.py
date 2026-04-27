589b9b1affa4683a52d86ba9fd180ccc10d60c80
oio0-0 <lolkin2268@gmail.com>
2026-04-27 19:47:38 +0300

    Update views.py

--- django_email_verification/views.py
+++ django_email_verification/views.py
@@ -1,23 +1,24 @@
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from django.contrib.auth import login
 
-from .confirm import verify_email_view, verify_email, verify_password_view, verify_password
+from .confirm import verify_email_view, verify_email, verify_password_view, verify_password, verify_email_update
 from .errors import NotAllFieldCompiled
 
-
 @verify_email_view
-def verify(request: WSGIRequest, token):
+def verify_email_page(request: WSGIRequest, token):
     try:
         template = settings.EMAIL_MAIL_PAGE_TEMPLATE
         success, user = verify_email(token)
+        user.save()
+
+        if success and user is not None:
+            login(request, user)
+        
         return render(request, template, {'success': success, 'user': user, 'request': request})
     except (AttributeError, TypeError):
         raise NotAllFieldCompiled('EMAIL_MAIL_PAGE_TEMPLATE field not found')
-
-
-verify_email_page = verify
-
 
 @verify_password_view
 def verify_password_page(request: WSGIRequest, token):
@@ -29,3 +30,18 @@ def verify_password_page(request: WSGIRequest, token):
         return render(request, settings.EMAIL_PASSWORD_CHANGE_PAGE_TEMPLATE, {'token': token, 'request': request})
     except (AttributeError, TypeError):
         raise NotAllFieldCompiled('EMAIL_PASSWORD templates field not found')
+
+@verify_email_update_view
+def verify_email_update_page(request: WSGIRequest, token):
+    try:
+        template = settings.EMAIL_MAIL_UPDATE_PAGE_TEMPLATE
+        success, user, new_email = verify_email_update(token)
+        user.email = new_email
+        user.save()
+
+        if success and user is not None:
+            login(request, user)
+        
+        return render(request, template, {'success': success, 'user': user, 'request': request})
+    except (AttributeError, TypeError):
+        raise NotAllFieldCompiled('EMAIL_MAIL_UPDATE_PAGE_TEMPLATE field not found')
